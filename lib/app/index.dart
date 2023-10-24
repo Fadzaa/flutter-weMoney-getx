1d29615cdd7a48b9e19e61e9aae1f4b15b60271c
Fadza <fadza0077@gmail.com>
2023-10-24 12:17:01 +0700

    feat: [FA] add new structure, add helper, and routing

---
+++ lib/app/index.dart
@@ -0,0 +1,19 @@
+// Splash Screen
+export './splash_screen/splash_screen_view.dart' show SplashScreenView;
+export './splash_screen/splash_screen_controller.dart' show SplashScreenController;
+export './splash_screen/splash_screen_binding.dart' show SplashScreenBinding;
+
+// Register Screen
+export './register_screen/register_screen_view.dart' show RegisterScreenView;
+export './register_screen/register_screen_controller.dart' show RegisterScreenController;
+export './register_screen/register_screen_binding.dart' show RegisterScreenBinding;
+
+// Login Screen 
+export './login_screen/login_screen_view.dart' show LoginScreenView;
+export './login_screen/login_screen_controller.dart' show LoginScreenController;
+export './login_screen/login_screen_binding.dart' show LoginScreenBinding;
+
+// Onboarding Screen
+export './onboarding_screen/onboarding_screen_view.dart' show OnboardingScreenView;
+export './onboarding_screen/onboarding_screen_controller.dart' show OnboardingScreenController;
+export './onboarding_screen/onboarding_screen_binding.dart' show OnboardingScreenBinding;
---
+++ lib/app/register_screen/register_screen_view.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:we_money_getx/app/splash_screen/splash_screen_controller.dart';
+
+class RegisterScreenView extends GetView<SplashScreenController> {
+  const RegisterScreenView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
