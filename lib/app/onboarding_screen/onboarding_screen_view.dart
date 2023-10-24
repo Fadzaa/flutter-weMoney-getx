fa437b9372e45a575eaa0a987df5f22d599f5cae
Fadza <fadza0077@gmail.com>
2023-10-25 00:44:06 +0700

    style: [FA] style onboarding-screen

--- lib/app/onboarding_screen/onboarding_screen_view.dart
+++ lib/app/onboarding_screen/onboarding_screen_view.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:introduction_screen/introduction_screen.dart';
 import 'package:we_money_getx/app/index.dart';
+import 'package:we_money_getx/common/helper/themes.dart';
 
 class OnboardingScreenView extends GetView<OnboardingScreenController> {
   OnboardingScreenView({super.key});
@@ -45,8 +46,6 @@ class OnboardingScreenView extends GetView<OnboardingScreenController> {
         key: introKey,
         globalBackgroundColor: Colors.white,
         allowImplicitScrolling: true,
-        autoScrollDuration: 3000,
-        infiniteAutoScroll: true,
         globalHeader: Align(
           alignment: Alignment.topRight,
           child: SafeArea(
@@ -56,108 +55,60 @@ class OnboardingScreenView extends GetView<OnboardingScreenController> {
             ),
           ),
         ),
-        globalFooter: SizedBox(
+        globalFooter: Container(
+          margin: EdgeInsets.fromLTRB(28, 0, 28, 50),
           width: double.infinity,
-          height: 60,
+          height: 45,
           child: ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              backgroundColor: primaryColor,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(7),
+              ),
+            ),
             child: const Text(
-              'Let\'s go right away!',
-              style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.bold),
+              'Next',
+              style: TextStyle(
+                  fontSize: 16.0,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white),
             ),
             onPressed: () => _onIntroEnd(context),
           ),
         ),
         pages: [
           PageViewModel(
-            title: "Fractional shares",
-            body:
-                "Instead of having to buy an entire share, invest any amount you want.",
-            image: _buildImage('img1.jpg'),
+            title: "Point Text",
+            body: "Quick money tranfer in wallet",
+            image: _buildImage('onboarding_image_1.png'),
             decoration: pageDecoration,
           ),
           PageViewModel(
-            title: "Learn as you go",
-            body:
-                "Download the Stockpile app and master the market with our mini-lesson.",
-            image: _buildImage('img2.jpg'),
+            title: "Point Text",
+            body: "Exchange different currencies easily",
+            image: _buildImage('onboarding_image_2.png'),
             decoration: pageDecoration,
           ),
           PageViewModel(
-            title: "Kids and teens",
-            body:
-                "Kids and teens can track their stocks 24/7 and place trades that you approve.",
-            image: _buildImage('img3.jpg'),
+            title: "Point Text",
+            body: "Always track your transactions",
+            image: _buildImage('onboarding_image_3.png'),
             decoration: pageDecoration,
-          ),
-          PageViewModel(
-            title: "Full Screen Page",
-            body:
-                "Pages can be full screen as well.\n\nLorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc id euismod lectus, non tempor felis. Nam rutrum rhoncus est ac venenatis.",
-            image: _buildFullscreenImage(),
-            decoration: pageDecoration.copyWith(
-              contentMargin: const EdgeInsets.symmetric(horizontal: 16),
-              fullScreen: true,
-              bodyFlex: 2,
-              imageFlex: 3,
-              safeArea: 100,
-            ),
-          ),
-          PageViewModel(
-            title: "Another title page",
-            body: "Another beautiful body text for this example onboarding",
-            image: _buildImage('img2.jpg'),
-            footer: ElevatedButton(
-              onPressed: () {
-                introKey.currentState?.animateScroll(0);
-              },
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.lightBlue,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(8.0),
-                ),
-              ),
-              child: const Text(
-                'FooButton',
-                style: TextStyle(color: Colors.white),
-              ),
-            ),
-            decoration: pageDecoration.copyWith(
-              bodyFlex: 6,
-              imageFlex: 6,
-              safeArea: 80,
-            ),
-          ),
-          PageViewModel(
-            title: "Title of last page - reversed",
-            bodyWidget: const Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text("Click on ", style: bodyStyle),
-                Icon(Icons.edit),
-                Text(" to edit a post", style: bodyStyle),
-              ],
-            ),
-            decoration: pageDecoration.copyWith(
-              bodyFlex: 2,
-              imageFlex: 4,
-              bodyAlignment: Alignment.bottomCenter,
-              imageAlignment: Alignment.topCenter,
-            ),
-            image: _buildImage('img1.jpg'),
-            reverse: true,
           ),
         ],
         onDone: () => _onIntroEnd(context),
         onSkip: () => _onIntroEnd(context), // You can override onSkip callback
-        showSkipButton: true,
+        showSkipButton: false,
+        showDoneButton: false,
         skipOrBackFlex: 0,
         nextFlex: 0,
         showBackButton: false,
+        showNextButton: false,
         //rtl: true, // Display as right-to-left
-        back: const Icon(Icons.arrow_back),
-        skip: const Text('Skip', style: TextStyle(fontWeight: FontWeight.w600)),
-        next: const Icon(Icons.arrow_forward),
-        done: const Text('Done', style: TextStyle(fontWeight: FontWeight.w600)),
+
+        // skip: const Text('Skip', style: TextStyle(fontWeight: FontWeight.w600)),
+        // next: const Icon(Icons.arrow_forward),
+        // done: const Text('Done', style: TextStyle(fontWeight: FontWeight.w600)),
         curve: Curves.fastLinearToSlowEaseIn,
         controlsMargin: const EdgeInsets.all(16),
         controlsPadding: const EdgeInsets.fromLTRB(8.0, 4.0, 8.0, 4.0),
@@ -167,12 +118,6 @@ class OnboardingScreenView extends GetView<OnboardingScreenController> {
           activeSize: Size(22.0, 10.0),
           activeShape: RoundedRectangleBorder(
             borderRadius: BorderRadius.all(Radius.circular(25.0)),
-          ),
-        ),
-        dotsContainerDecorator: const ShapeDecoration(
-          color: Colors.black87,
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.all(Radius.circular(8.0)),
           ),
         ),
       ),
