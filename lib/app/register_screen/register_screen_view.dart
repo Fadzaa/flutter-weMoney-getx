5459aabf07e5bd7680bd07797f75a13aee14f8cc
Cia <zazaskiaa@gmail.com>
2023-10-29 11:23:34 +0700

    style: [ZJ] style login-page

--- lib/app/register_screen/register_screen_view.dart
+++ lib/app/register_screen/register_screen_view.dart
@@ -1,12 +1,256 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:we_money_getx/app/splash_screen/splash_screen_controller.dart';
+import 'package:we_money_getx/common/helper/themes.dart';
 
 class RegisterScreenView extends GetView<SplashScreenController> {
   const RegisterScreenView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: secondaryColor,
+      body: Padding(
+        padding: const EdgeInsets.only(left: 24),
+
+        child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+
+              SizedBox(
+                height:34,
+              ),
+
+              Row(
+                children: [
+                  ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                        fixedSize: Size(35, 35),
+                        backgroundColor:Color(0XFFDFDFDF)
+                    ),
+                    child: null,
+                  ),
+
+                  SizedBox(
+                    width: 93,
+                  ),
+
+                  Image.asset("assets/logo.png"),
+                ],
+              ),
+
+              SizedBox(
+                height: 38,
+              ),
+
+              Text("Sign Up to WeMoney",
+                style: GoogleFonts.montserrat(
+                    fontSize: 22,
+                    fontWeight: FontWeight.w600
+                ),
+              ),
+
+              SizedBox(
+                height: 29,
+              ),
+
+              Container(
+                width: 305,
+                height: 54,
+                child: TextField(
+                  decoration: InputDecoration(
+                    // contentPadding: EdgeInsets.symmetric(horizontal: 22),
+                      prefixIcon: SvgPicture.asset('assets/person-icon.svg'),
+                      hintText: "Name",
+                      hintStyle: GoogleFonts.poppins(
+                        color: secondaryTextColor,
+                      ),
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(6)
+                      )
+                  ),
+                ),
+              ),
+
+              SizedBox(
+                height: 16,
+              ),
+
+              Container(
+                width: 305,
+                height: 54,
+                child: TextField(
+                  decoration: InputDecoration(
+                      prefixIcon: SvgPicture.asset('assets/email-icon.svg'),
+                      hintText: "Email",
+                      hintStyle: GoogleFonts.poppins(
+                        color: secondaryTextColor,
+                      ),
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(6)
+                      )
+                  ),
+                ),
+              ),
+
+              SizedBox(
+                height: 16,
+              ),
+
+              Container(
+                width: 305,
+                height: 54,
+                child: TextField(
+                  decoration: InputDecoration(
+                      prefixIcon: SvgPicture.asset('assets/lock-icon.svg'),
+                      hintText: "Password",
+                      hintStyle: GoogleFonts.poppins(
+                        color: secondaryTextColor,
+                      ),
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(6)
+                      )
+                  ),
+                ),
+              ),
+
+              SizedBox(
+                height: 11,
+              ),
+
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 24),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Text("Forget Password?",
+                      style: GoogleFonts.poppins(
+                          fontWeight: FontWeight.w600,
+                          fontSize: 13,
+                          color: primaryColor
+                      ),),
+                  ],
+                ),
+              ),
+
+              SizedBox(
+                height: 21,
+              ),
+
+              ElevatedButton(onPressed: (){},
+                  style: ElevatedButton.styleFrom(
+                      fixedSize: Size(305, 45),
+                      backgroundColor: primaryColor
+                  ),
+                  child: Text(
+                    "Sign Up",
+                    style: GoogleFonts.poppins(
+                        color: whiteTextColor
+                    ),
+                  )
+              ),
+
+              SizedBox(
+                height: 41,
+              ),
+
+              Row(
+                children: [
+                  Expanded(
+                    child: Divider(
+                      color: secondaryTextColor,
+                    ),
+                  ),
+                  Text("or continue with"),
+                  Expanded(
+                    child: Divider(
+                      color: secondaryTextColor,
+                    ),
+                  ),
+                ],
+              ),
+
+              SizedBox(
+                height: 41,
+              ),
+
+              ElevatedButton(onPressed: (){},
+                  style: ElevatedButton.styleFrom(
+                      fixedSize: Size(302, 53),
+                      backgroundColor: secondaryColor
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      SvgPicture.asset("assets/google-icon.svg",
+                          width: 29,
+                          height: 29
+                      ),
+
+                      SizedBox(
+                        width: 16,
+                      ),
+
+                      Text(
+                        "Sign In with Google",
+                        style: GoogleFonts.poppins(
+                            color: primaryTextColor,
+                            fontSize: 15,
+                            fontWeight: FontWeight.w600
+                        ),
+                      ),
+                    ],
+                  )
+              ),
+
+              SizedBox(
+                height: 15,
+              ),
+
+              ElevatedButton(onPressed: (){},
+                  style: ElevatedButton.styleFrom(
+                      fixedSize: Size(302, 53),
+                      backgroundColor: secondaryColor
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      SvgPicture.asset("assets/fb-icon.svg",
+                          width: 29,
+                          height: 29
+                      ),
+
+                      SizedBox(
+                        width: 16,
+                      ),
+
+                      Text(
+                        "Sign In with Facebook",
+                        style: GoogleFonts.poppins(
+                            color: primaryTextColor,
+                            fontSize: 15,
+                            fontWeight: FontWeight.w600
+                        ),
+                      ),
+                    ],
+                  )
+              ),
+
+              SizedBox(
+                height: 33,
+              ),
+
+              Text("Already have an account? Sign In",
+                style: GoogleFonts.poppins(
+                    fontWeight: FontWeight.w600,
+                    fontSize: 10
+                ),)
+            ]
+        ),
+      ),
+    );
   }
 }
