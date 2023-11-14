263e3776f63158529da679b35142bc6c213a3029
Fadza <fadza0077@gmail.com>
2023-11-15 01:01:08 +0700

    feat: [FA] add components set balance limit

--- lib/app/features/balance_detail_page/detail_balance_view.dart
+++ lib/app/features/balance_detail_page/detail_balance_view.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:get/get.dart';
+import 'package:we_money_getx/app/features/home_page/components/balance_overview.dart';
+import 'package:we_money_getx/app/features/home_page/components/total_balance.dart';
+import 'package:we_money_getx/common/helper/index.dart';
 
 class DetailBalanceView extends GetView {
   const DetailBalanceView({super.key});
@@ -7,10 +11,38 @@ class DetailBalanceView extends GetView {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          
-        ],
+      backgroundColor: Colors.white,
+      body: Padding(
+        padding: const EdgeInsets.fromLTRB(18, 25, 18, 0),
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                SvgPicture.asset("assets/icon_arrow_back.svg"),
+
+                Text("DETAIL BALANCE", style: tsHeading,),
+
+                const SizedBox()
+
+              ],
+            ),
+
+            SizedBox(height: 15,),
+
+            const TotalBalance(),
+
+            SizedBox(height: 25,),
+
+            const BalanceOverview(),
+
+            SizedBox(height: 10,),
+
+
+
+
+          ],
+        ),
       ),
     );
   }
