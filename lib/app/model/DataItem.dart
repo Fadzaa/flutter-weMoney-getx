8194a4755cf88f725b7ca415da50dacc74dde5d5
dudedod67 <122068251+dudedod67@users.noreply.github.com>
2023-11-18 10:37:42 +0700

    feat: [DNA] add data

---
+++ lib/app/model/DataItem.dart
@@ -0,0 +1,11 @@
+class DateItem {
+  final String month;
+  final String day;
+  final String dayOfWeek;
+
+  DateItem({
+    required this.month,
+    required this.day,
+    required this.dayOfWeek,
+  });
+}
